b0bbee327305e1d0ba337c5be0f17801fff7fbc1
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2024-12-27 14:35:56 +0100

    exynos_boot: cleanup for secure SoCs
    This would read invalid memory regions, and as such drop us to fastboot.

--- app/exynos_boot/exynos_boot.c
+++ app/exynos_boot/exynos_boot.c
@@ -52,69 +52,20 @@ static void exynos_boot_task(const struct app_descriptor *app, void *args)
 {
 	unsigned int rst_stat = readl(EXYNOS_POWER_RST_STAT);
 	struct exynos_gpio_bank *bank = (struct exynos_gpio_bank *)EXYNOS9830_GPA0CON;
-	int gpio = 3;	/* Volume Up */
+	int gpio = 4;	/* Volume down */
 	int val;
 
-	if (*(unsigned int *)DRAM_BASE != 0xabcdef) {
-		printf("Running on DRAM by TRACE32: skip auto booting\n");
-		start_usb_gadget();
-		return;
-	}
-
-	printf("AP revision is EVT%d\n", s5p_chip_rev.main);
-	print_lcd(FONT_RED, FONT_BLACK, "AP revision is EVT%d", s5p_chip_rev.main);
-	if (s5p_chip_rev.main != (unsigned int)dfd_get_revision()) {
-		printf("Using invalid BL2!\n");
-		printf("Current BL2 is for EVT%d\n", dfd_get_revision());
-		print_lcd(FONT_RED, FONT_BLACK, "Using invalid BL2!");
-		print_lcd(FONT_RED, FONT_BLACK, "Current BL2 is for EVT%d", dfd_get_revision());
-		start_usb_gadget();
-		return;
-	}
-
-	/* Volume up set Input & Pull up */
+	/* Volume down set Input & Pull up */
 	exynos_gpio_set_pull(bank, gpio, GPIO_PULL_UP);
 	exynos_gpio_cfg_pin(bank, gpio, GPIO_INPUT);
 	mdelay(50);
 	val = exynos_gpio_get_value(bank, gpio);
-	if (!is_first_boot() ||
-			(rst_stat & (WARM_RESET | LITTLE_WDT_RESET | BIG_WDT_RESET))) {
-		dfd_set_dump_en(0);
-		sdm_encrypt_secdram();
-		goto ramdump;
-	}
-	if ((readl(CONFIG_RAMDUMP_SCRATCH) == CONFIG_RAMDUMP_MODE) &&
-		get_charger_mode() == 0) {
-		sdm_encrypt_secdram();
-		goto ramdump;
-	}
-
-	if (need_do_fastboot) {
-		sdm_encrypt_secdram();
-		print_fastboot_reason();
-		goto ramdump;
-	}
 
 	if (!val)
 		start_usb_gadget();
+	else
+		cmd_boot(0, 0);
 
-#ifdef RAMDUMP_MODE_OFF
-	dfd_set_dump_en(0);
-	set_debug_level("low");
-#else
-	dfd_set_dump_en(1);
-	set_debug_level("mid");
-#endif
-	cmd_boot(0, 0);
-	return;
-ramdump:
-#ifdef RAMDUMP_MODE_OFF
-	cmd_boot(0, 0);
-#else
-	debug_store_ramdump();
-	dfd_set_dump_en(0);
-	start_usb_gadget();
-#endif
 	return;
 }
 
