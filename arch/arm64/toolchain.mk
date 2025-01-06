c06baba6d9fdb42275e83a1c9cc352470c7e765a
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-01-06 15:21:04 +0000

    Change default cross compiler
    The compiler currently used (aarch64-elf version 5.3) doesn't support PIC and makes devices freeze on the Samsung logo when attempting to boot the compiled binaries.
    
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- arch/arm64/toolchain.mk
+++ arch/arm64/toolchain.mk
@@ -3,13 +3,13 @@ ARCH_arm64_TOOLCHAIN_INCLUDED := 1
 
 ifndef ARCH_arm64_TOOLCHAIN_PREFIX
 	ifeq ($(TOOLCHAIN_PREFIX),)
-		ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-elf-
+		ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 		FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
 		ifeq ($(FOUNDTOOL),)
-			ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-elf-
+			ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 			FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
 			ifeq ($(FOUNDTOOL),)
-				ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-elf-
+				ARCH_arm64_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 				FOUNDTOOL=$(shell which $(ARCH_arm64_TOOLCHAIN_PREFIX)gcc)
 				ifeq ($(FOUNDTOOL),)
 					$(error cannot find toolchain, please set ARCH_arm64_TOOLCHAIN_PREFIX or add it to your path)
