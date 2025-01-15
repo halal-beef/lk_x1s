e54e9a2602559242be55ccc44fdbb1399d75ef53
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-01-15 21:59:52 +0100

    project: Add z3s project
    z3s -> Galaxy S20 Ultra 5G.
    Top-end flagship phone from Samsung, based on Exynos990.
    
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

---
+++ project/z3s.mk
@@ -0,0 +1,45 @@
+ARCH := arm64
+ARM_CPU := cortex-a53
+TARGET := z3s
+
+WITH_KERNEL_VM := 0
+WITH_LINKER_GC := 1
+
+GLOBAL_DEFINES += \
+	SYSPARAM_ALLOW_WRITE=1 \
+	WITH_LIB_CONSOLE=1 \
+	INPUT_GPT_AS_PT=0 \
+	GPT_PART=0 \
+	BOOTLOADER_FB_ADDRESS=0xf1000000 \
+	LCD_WIDTH=1440 \
+	LCD_HEIGHT=3200
+export INPUT_GPT_AS_PT=0
+export GPT_PART=0
+
+MODULES += \
+	app/exynos_boot
+
+MODULE_DEPS += \
+	platform/exynos \
+	lib/gpt \
+	lib/font \
+	lib/logo \
+	lib/libavb \
+	lib/cksum \
+	lib/fdt \
+	lib/ufdt \
+	lib/fdtapi \
+	lib/ab_update \
+	lib/lock \
+	lib/fastboot \
+	lib/block \
+	dev/scsi \
+	dev/mmc \
+	dev/rpmb \
+	dev/adc \
+	dev/pmucal \
+	dev/speedy \
+	dev/power/pmic/s2mps_19_22 \
+	dev/power/pmic/s2dos05 \
+	dev/battery/charger \
+	dev/debug \
