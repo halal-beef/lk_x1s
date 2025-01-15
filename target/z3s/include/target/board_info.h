a5b35ba68eae17375a530111cb7a34e19135ac7f
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-01-15 21:59:52 +0100

    project: Add z3s project
    z3s -> Galaxy S20 Ultra 5G.
    Top-end flagship phone from Samsung, based on Exynos990.
    
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

---
+++ target/z3s/include/target/board_info.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright@ Samsung Electronics Co. LTD
+ *
+ * This software is proprietary of Samsung Electronics.
+ * No part of this software, either material or conceptual may be copied or distributed, transmitted,
+ * transcribed, stored in a retrieval system or translated into any human or computer language in any form by any means,
+ * electronic, mechanical, manual or otherwise, or disclosed
+ * to third parties without the express written permission of Samsung Electronics.
+ */
+
+#ifndef __BOARD_INFO_H__
+#define __BOARD_INFO_H__
+
+#define CONFIG_BOARD_NAME	"z3s"
+#define CONFIG_BOARD_X1S
+
+#define CONFIG_BOARD_ID		0x10
+#define CONFIG_SUB_PMIC_S2DOS05
+#define CONFIG_UFS_BOARD_TYPE	1      /* smdk : 0, universal : 1 */
+
+#define CONFIG_USE_RPMB
+#define CONFIG_USE_F2FS
+#define RPMB_BLOCK_PER_PARTITION      512
+
+#define BOOT_IMG_HDR_V2
+#define CONFIG_DTB_IN_BOOT
+#define CONFIG_RAMDISK_IN_BOOT
+
+#define CONFIG_GET_B_REV_FROM_ADC
+#define CONFIG_GET_B_REV_FROM_GPIO
+
+#define VOLDOWN_GPIOCON EXYNOS9830_GPA0CON
+#define VOLDOWN_BIT	4
+#endif /*__BOARD_INFO_H__ */
