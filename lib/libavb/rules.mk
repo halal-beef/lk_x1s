20810c7adb50d57be9637bf380d3b6cad6b1a3db
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2025-02-02 18:27:46 +0100

    libavb: Remove stray avb_exynos stuff
    Why was this here? Anyway, upstream added abort(), so no need for this.
    
    Signed-off-by: Igor Belwon <igor.belwon@mentallysanemainliners.org>

--- lib/libavb/rules.mk
+++ lib/libavb/rules.mk
@@ -22,7 +22,6 @@ MODULE_SRCS += \
 	$(LOCAL_DIR)/avb_rsa.c \
 	$(LOCAL_DIR)/avb_sha256.c \
 	$(LOCAL_DIR)/avb_sha512.c \
-	$(LOCAL_DIR)/avb_cmdline.c \
-	$(LOCAL_DIR)/avb_exynos.c
+	$(LOCAL_DIR)/avb_cmdline.c
 
 include make/module.mk
